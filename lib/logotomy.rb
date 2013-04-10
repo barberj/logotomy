bfc8ebce586eb358f7b992092d0a6c16992c1df3
barberj <barber.justin@gmail.com>
2013-04-10 15:23:49 -0400

    I said silence!

--- lib/logotomy.rb
+++ lib/logotomy.rb
@@ -1,5 +1,36 @@
 require "logotomy/version"
 
 module Logotomy
-  # Your code goes here...
+  class ActiveRecord::LogSubscriber
+    def sql event
+    end
+  end
+
+  class ActionController::LogSubscriber
+    def start_processing event
+    end
+
+    def process_action event
+    end
+
+    def halted_callback event
+    end
+
+    def send_file event
+    end
+
+    def redirect_to event
+    end
+
+    def send_data event
+    end
+  end
+
+  class ActionView::LogSubscriber
+    def render_template event
+    end
+
+    alias :render_partial :render_template
+    alias :render_collection :render_template
+  end
 end
